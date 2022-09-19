19743a5eb67a8fdb6971ec503183811a5d96e0f4
Gourav Saini <113007728+gauravs1650@users.noreply.github.com>
2022-09-19 12:27:26 +0530

    Add files via upload

---
+++ array program by using loop.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x[10],i;
+	for(int i=0;i<10;i++)
+	{
+		cout<<"enter data";
+		cin>>x[i];
+	}
+	for(int i=0;i<10;i++)
+	{
+		cout<<x[i]<<endl;
+	}
+	return 0;
+}
